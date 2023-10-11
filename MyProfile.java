1836dfbdf97b88876d51348cec3bb1e091cda752
SharifahHana <147573070+SharifahHana@users.noreply.github.com>
2023-10-11 12:43:14 +0800

    Add files via upload

---
+++ MyProfile.java
@@ -0,0 +1,18 @@
+package Lab1;
+
+public class MyProfile {	
+	
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	System.out.println ("............................................."); 
+	System.out.println (".            Hana's profile <3              ."); 
+	System.out.println ("............................................."); 
+	System.out.println ("Name:Sharifah Hana Maisarah binti Syed Omar"); 
+	System.out.println ("Matric: 222635"); 
+	System.out.println ("State/Province: Selangor"); 
+	System.out.println ("Is this program your first choice?: Yes"); 
+	System.out.println ("Expectations from this course: To enjoy and learn valuable knowledge"); 
+}	
+
+
+}
