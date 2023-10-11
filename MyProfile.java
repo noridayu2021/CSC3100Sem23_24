58545809c9ac3d1583b61711bb7f60a2e0e26997
Acer01 <Acer01@Acer03>
2023-10-11 16:30:58 +0800

    version2

--- MyProfile.java
+++ MyProfile.java
@@ -11,7 +11,7 @@ public class MyProfile {
 		System.out.println("Name: Choo En Yi");//to print message
 		System.out.println("Matric number:222333");//to print message
 		System.out.println("State/Province: Perak");//to print message
-		System.out.println("Which part????")// reply me
+		System.out.println("Which part?????")// reply me
 		System.out.println("Is this program your first choice?: Sure!");//to print message
 		System.out.println("Expectation from this course: Hope I ace in this course and enjoy the journey!");//to print message
 	}
